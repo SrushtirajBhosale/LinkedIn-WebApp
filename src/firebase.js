c25ae15d1bae9ebde85f7df61b2fe24468b47afa
Srushtiraj Bhosale <srushtiraj777@gmail.com>
2023-06-22 19:31:34 +0530

    Application Updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2WasmWmOCrLI0ZV4u4ENhfbyUeYZFYiY",
-  authDomain: "linked-n-eae5a.firebaseapp.com",
-  databaseURL: "https://linked-n-eae5a-default-rtdb.firebaseio.com",
-  projectId: "linked-n-eae5a",
-  storageBucket: "linked-n-eae5a.appspot.com",
-  messagingSenderId: "1169066772",
-  appId: "1:1169066772:web:27bed66a361ce069761001",
-  measurementId: "G-9MT3RWV19X"
+  apiKey: "AIzaSyC6tKH-wjeXoJs7JMRIt9EGXu3Gw3dY_sM",
+  authDomain: "social-group-f2030.firebaseapp.com",
+  databaseURL: "https://social-group-f2030-default-rtdb.firebaseio.com",
+  projectId: "social-group-f2030",
+  storageBucket: "social-group-f2030.appspot.com",
+  messagingSenderId: "901630834637",
+  appId: "1:901630834637:web:01194f484b42c06a9f7684",
+  measurementId: "G-HZQ3F3JE4X"
 };
 
 const app = initializeApp(firebaseConfig);
